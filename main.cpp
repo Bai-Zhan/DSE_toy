a4b75bdceea87a10980f6abf076b2e3f433d163e
Bai-Zhan <baizhan.phy@hotmail.com>
2021-08-10 17:31:37 +0800

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+    > File Name: main.cpp
+    > Author: Zhan Bai
+    > Mail: 
+    > Created Time: Tue 10 Aug 2021 05:30:17 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	printf("hello world!\n");
+	return 0;
+}
